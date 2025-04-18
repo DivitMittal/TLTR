16497a9125ff5e2c1a060040a84959faaaed774a
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-04-18 06:51:31 +0530

    feat: added nix support with flakes & development ephemeral environment with treefmt

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "TLTR flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {flake-parts, ...} @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = import inputs.systems;
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+      perSystem = _: {
+        treefmt = {
+          flakeCheck = false; # handled via pre-commit
+
+          programs = {
+            #typos.enable = true;
+            ## Nix
+            alejandra.enable = true;
+            deadnix.enable = true;
+            statix.enable = true;
+            ## JSON
+            prettier.enable = true;
+          };
+
+          projectRootFile = "flake.nix";
+        };
+      };
+    };
+}
