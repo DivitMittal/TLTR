c90e7eced3e4fcb50fa865c32e0f162a6c426352
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-06-09 18:02:34 +0530

    feat: add flake.nix customLib & therefore OS-nixCfg input

--- flake.nix
+++ flake.nix
@@ -1,5 +1,14 @@
 {
   description = "TLTR flake";
+  outputs = inputs: let
+    inherit (inputs.flake-parts.lib) mkFlake;
+    specialArgs.customLib = inputs.OS-nixCfg.lib;
+  in
+    mkFlake {inherit inputs specialArgs;} {
+      systems = builtins.import inputs.systems;
+      imports = [./flake];
+    };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
@@ -8,28 +17,28 @@
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-  };
-
-  outputs = inputs: let
-    inherit (inputs.flake-parts.lib) mkFlake;
-  in
-    mkFlake {inherit inputs;} {
-      systems = builtins.import inputs.systems;
-      imports = [inputs.treefmt-nix.flakeModule];
-      perSystem.treefmt = {
-        projectRootFile = "flake.nix";
-
-        flakeCheck = false;
-
-        programs = {
-          #typos.enable = true;
-          ## Nix
-          alejandra.enable = true;
-          deadnix.enable = true;
-          statix.enable = true;
-          ## JSON
-          prettier.enable = true;
-        };
+    actions-nix = {
+      url = "github:nialov/actions.nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-parts.follows = "flake-parts";
+        pre-commit-hooks.follows = "pre-commit-hooks";
       };
     };
+    pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    OS-nixCfg = {
+      url = "github:DivitMittal/OS-nixCfg";
+      inputs = {
+        systems.follows = "systems";
+        nixpkgs.follows = "nixpkgs";
+        flake-parts.follows = "flake-parts";
+        treefmt-nix.follows = "treefmt-nix";
+        actions-nix.follows = "actions-nix";
+        pre-commit-hooks.follows = "pre-commit-hooks";
+      };
+    };
+  };
 }
