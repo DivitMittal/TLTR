8fc19d59201a1d7636d9c472e4e5ed46da299581
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-12-18 14:32:43 +0530

    ci(keymap-drawer): fix persistence of credential across workflow

--- flake/actions/keymap-drawer.nix
+++ flake/actions/keymap-drawer.nix
@@ -1,4 +1,4 @@
-{common-actions, ...}: {
+_: {
   flake.actions-nix.workflows.".github/workflows/keymap-drawer.yml" = {
     on = rec {
       push = {
@@ -12,38 +12,43 @@
       permissions = {
         contents = "write";
       };
-      steps =
-        common-actions
-        ++ [
-          {
-            name = "Install uv with caching";
-            uses = "astral-sh/setup-uv@main";
-            "with" = {
-              enable-cache = "true";
-            };
-          }
-          {
-            name = "Install keymap-drawer";
-            run = "uv tool install keymap-drawer";
-          }
-          {
-            name = "Run keymap-drawer";
-            run = "keymap draw ./keymap-drawer/tltr.yml 1> assets/tltr.svg";
-          }
-          {
-            name = "Push to repo";
-            run = ''
-              git config --global user.name "GitHub Actions Bot"
-              git config --global user.email bot@github.com
-              git add .
-              git commit -m "chore: update keymap-drawer assets"
-              git push origin master
-            '';
-            env = {
-              GITHUB_TOKEN = "\${{ secrets.GITHUB_TOKEN }}";
-            };
-          }
-        ];
+      steps = [
+        {
+          name = "Checkout repo";
+          uses = "actions/checkout@main";
+          "with" = {
+            fetch-depth = 1;
+          };
+        }
+        {
+          name = "Install uv with caching";
+          uses = "astral-sh/setup-uv@main";
+          "with" = {
+            enable-cache = "true";
+          };
+        }
+        {
+          name = "Install keymap-drawer";
+          run = "uv tool install keymap-drawer";
+        }
+        {
+          name = "Run keymap-drawer";
+          run = "keymap draw ./keymap-drawer/tltr.yml 1> assets/tltr.svg";
+        }
+        {
+          name = "Push to repo";
+          run = ''
+            git config --global user.name "GitHub Actions Bot"
+            git config --global user.email bot@github.com
+            git add .
+            git commit -m "chore: update keymap-drawer assets"
+            git push origin master
+          '';
+          env = {
+            GITHUB_TOKEN = "\${{ secrets.GITHUB_TOKEN }}";
+          };
+        }
+      ];
     };
   };
 }
