a54b6ded22bb90dc436dcbc4ac31a5942338b0c3
Divit Mittal <64.69.76.69.74.m@gmail.com>
2026-07-01 21:35:44 +0530

    fix(nix): use stdenvNoCC.hostPlatform.isDarwin for Darwin check

--- flake/packages.nix
+++ flake/packages.nix
@@ -1,6 +1,6 @@
 {lib, ...}: {
   perSystem = {pkgs, ...}: {
-    packages = lib.optionalAttrs pkgs.stdenv.isDarwin {
+    packages = lib.optionalAttrs pkgs.stdenvNoCC.hostPlatform.isDarwin {
       macos-key-logger = pkgs.writers.writePython3Bin "macos-key-logger" {
         libraries = [pkgs.python3Packages.pyobjc-framework-Quartz];
         doCheck = false;
